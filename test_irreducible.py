5d7f870c348776f409f62058d4c451a6f069855e
Ralph Hutchison <ralf.hutchison@gmail.com>
2016-04-28 16:19:47 -0700

    refactored garrett's irreducibility tests.

--- test_irreducible.py
+++ test_irreducible.py
@@ -2,10 +2,91 @@ import unittest
 from irreducibility_1 import *
 from numpy import poly1d
 from practical_sieve import odd_p_under_100 as ppp
+from random import choice, shuffle
+from poly_divmod import reduce_prime_poly as rpp
+from pickle import dump, load
+
+def three_digit(n):
+    """ return 3 character string 
+    last 3 digits of n, padded
+    with zeroes if necessary"""
+
+    third = str(n%10)
+    second = str((n%100)/10)
+    first = str((n%1000)/100)
+    return first + second + third
 
 class testIrreducibility1(unittest.TestCase):
     def setUp(self):
-        pass
+
+        flag= 0
+        
+        # create a bunch of test cases
+        if flag == 0: 
+            print "entering setup"
+            # fun fact:  if p is 3 mod 4 then x^2 +
+            # 1 is irreducible mod p.
+
+            l = [p for p in ppp if p %4 ==3]
+            l = l[:4]
+            f = poly1d([1,0,1])
+            data = []
+            for p in l:
+                d = choice(range(3))+1
+                g = self.random_p_poly(d,p)
+                h = rpp(f*g,p)
+                data.append((f,h,p))
+
+
+            # The idea for each of these blocks of
+            # tests is to generate a polynomial
+            # which is 
+
+            l = [p for p in ppp if p %3 ==2]
+            l = l[:4]
+            f = poly1d([1,1,1])
+            data = []
+            for p in l:
+                d = choice(range(3))+1
+                g = self.random_p_poly(d,p)
+                h = rpp(f*g,p)
+                data.append((f,h,p))
+
+            l = [p for p in ppp if p%5 ==1 or p%5 == 4]
+            l = l[:4]
+            f = poly1d([1,1,1,1])
+            data = []
+            for p in l:
+                d = choice(range(3))+1
+                g = self.random_p_poly(d,p)
+                h = rpp(f*g,p)
+                data.append((f,h,p))
+
+
+            l = [p for p in ppp if p%7 ==3 or p%7 == 5]
+            l = l[:4]
+            f = poly1d([1]*7)
+            data = []
+            for p in l:
+                d = choice(range(3))+1
+                g = self.random_p_poly(d,p)
+                h = rpp(f*g,p)
+                data.append((f,h,p))
+
+
+            s = three_digit(choice(range(1000)))
+            fname = 'data/datadump'+s
+            f = open(fname,'w')
+            dump(data,f)
+            print "test data [(f,g,primes)] dumped in", fname
+            self.data = data
+        else:
+            # load a bunch of test cases from file
+            fname = 'data/datadump964'
+            f = open(fname,'r')
+            data = load(f)
+            print "loading file: ", fname
+            self.data = data
 
     def tearDown(self):
         pass
@@ -22,36 +103,33 @@ class testIrreducibility1(unittest.TestCase):
     # example, with proof, see
     # irreducible_examples.pdf in this
     # directory. 
-    
-    def testGarrett_1_0_3(self):
-        
-        # fun fact:  if p is 3 mod 4 then x^2 +
-        # 1 is irreducible mod p. 
 
-        l = [p for p in ppp if p %4 ==3]
+    def random_p_poly(self,d,p):
 
-        f = poly1d([1,0,1])
-        
-        for p in l:
+        # first create a list of random residues
+        cs = []
+        residues = range(p)
+        cs = [choice(residues) for term in range(d)]
+        # add one which is not zero
+        cs.append(choice(range(p-1)) + 1)
+        # put it at the front
+        cs.reverse()
+        return (poly1d(cs))
+
+    def testGarrett(self):
+        print "running tests from http://www.math.umn.edu/~garrett/"
+        print "see ./irreducible_examples.pdf"
+
+        print_flag = True
+        for (f,g,p) in self.data:
             self.assertTrue(is_irreducible_1(f,p))
-
-    def testGarrett_1_0_4(self):
-        l = [p for p in ppp if p %3 ==2]
-        f = poly1d([1,1,1])
-        for p in l:
-            self.assertTrue(is_irreducible_1(f,p))
-
-    def testGarrett_1_0_5(self):
-        l = [p for p in ppp if p%5 ==1 or p%5 == 4]
-        f = poly1d([1,1,1,1])
-        for p in l:
-            self.assertTrue(is_irreducible_1(f,p))
-
-    def testGarrett_1_0_6(self):
-        l = [p for p in ppp if p%7 ==3 or p%7 == 5]
-        f = poly1d([1]*7)
-        for p in l:
-            self.assertTrue(is_irreducible_1(f,p))
+            h = rpp(f*g,p)
+            self.assertFalse(is_irreducible_1(h,p))
+            if print_flag:
+                print f
+                print "is irreducible  mod ",p
+                print h
+                print "is reducible mod ",p
 
 
 if __name__ == '__main__':
